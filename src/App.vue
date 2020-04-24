0b750f34da2601834555352ed78befc762f92d70
ryanyuyu <ryanyuyu42@gmail.com>
2020-04-24 17:11:11 -0500

    Previous installation was buried a folder too deep. Hoist things up.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App (which will eventually be the Dnd Encounter Builder)"/>
   </div>
 </template>
 
