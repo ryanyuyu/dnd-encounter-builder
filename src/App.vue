7b43cbfa1ba468012218bab178c0e368991177b5
ryanyuyu <ryanyuyu42@gmail.com>
2020-04-24 22:49:04 -0500

    Remove some helloworld stuff. Add sass to build.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App (which will eventually be the Dnd Encounter Builder)"/>
-  </div>
+    <div id="app">
+        <h1>DnD 5E Encounter Builder</h1>
+        <div class="enemy-section">
+            <h2>Enemies</h2>
+
+            <enemy-input-card v-for="(enemy, index) in enemyParty" :key="index"></enemy-input-card>
+            <button @click="addEnemy">Add Enemy Type</button>
+        </div>
+        <div class="player-section">
+            <h2>Players</h2>
+        </div>
+        <div class="result-section">
+            <h2>Encounter Estimates</h2>
+            <label>Difficulty</label>
+            {{ result.difficulty }}
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EnemyInputCard from "./components/EnemyInputCard.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data: function () {
+        return {
+            enemyParty: [{name: "Goblin", cr: ".25"}],
+            playerParty: [],
+            result: {
+                difficulty: 'Default Difficulty'
+            }
+        };
+    },
+    methods: {
+        addEnemy(enemy) {
+            this.enemyParty.push(enemy || {});
+        }
+    },
+    components: {
+        EnemyInputCard
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/EnemyInputCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="enemy-input-card">
+        <div class="input-group">
+            <label>Name</label>
+            <input v-model="name" />
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'EnemyInputCard',
+    data: function () {
+        return {
+            name: '',
+            cr: '',
+            count: 0,
+            // TODO some logic and/or prop for isRelevant
+        };
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+// Color constants
+$card-bg-color: #eeeeee;
+$label-color: #111111;
+
+// Padding constants
+$padding: 2px;
+
+.enemy-input-card {
+    display: flex;
+}
+
+label {
+    color: $label-color;
+}
+
+input {
+    padding: $padding;
+    border-radius: $padding;
+}
+
+</style>
